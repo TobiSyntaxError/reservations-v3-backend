63531581e56f7340a2082d6cde0ee051038dae67
Daniel <lohrengeldaniel@gmail.com>
2025-12-28 00:23:05 +0100

    testarray

--- src/api/views.py
+++ src/api/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 
 def status(request):
-    return JsonResponse({"authors": ["Tobias"]})
+    return JsonResponse({"authors": ["Tobias", "Daniel"]})
+    
 
 def health(request):
     return JsonResponse({"live": True, "ready": True})
