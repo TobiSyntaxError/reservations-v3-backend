522271c33ace9388c1c9907858342dddae56aac4
Tobias <tobiaskipping@outlook.com>
2025-12-30 00:18:52 +0100

    changed implementation of StatusView

--- src/api/views.py
+++ src/api/views.py
@@ -1,9 +1,26 @@
 from django.http import JsonResponse
 
-def status(request):
-    return JsonResponse({"authors": ["Tobias", "Daniel", "TestUser"]})
-    
+from __future__ import annotations
+from typing import Any
+from django.http import JsonResponse, HttpRequest
+from django.views import View
 
-def health(request):
-    #bitte funktioniert
-    return JsonResponse({"live": True, "ready": True})
+class StatusView(View):
+    AUTHORS = ["Tobias Kipping", "Daniel Lohrengel"]
+    API_VESION = "3.0.0"
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> JsonResponse:
+        return JsonResponse (
+            {
+                "authors": self.AUTHORS,
+                "api_version": self.API_VESION
+            }
+
+        )
+
+
+#def status(request):
+#    return JsonResponse({"authors": ["Tobias", "Daniel", "TestUser"]})
+#
+#def health(request):
+#    return JsonResponse({"live": True, "ready": True})
