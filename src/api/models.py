3aa3a4898280bfe73a2f5fff3afa1c868a430148
Tobias <tobiaskipping@outlook.com>
2026-01-01 22:32:46 +0100

    implemented db-model and executed migration

--- src/api/models.py
+++ src/api/models.py
@@ -1,2 +1,18 @@
 import uuid
 from django.db import models
+
+class Reservations(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    room_id = models.UUIDField()
+    from_date = models.DateField()
+    to_date = models.DateField()
+    delete_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["room_id", "from_date", "to_date"]),
+            models.Index(fields=["delete_at"])
+        ]
+
+    def __str__(self) -> str:
+        return f"Reservatio({self.id})"
